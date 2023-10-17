2705301f240ccfde84ee1a12f42bfdeb16a74db7
felipesembay <felipesembay91@gmail.com>
2023-10-16 21:10:58 -0300

    Tag GA4

--- tableau.py
+++ tableau.py
@@ -105,46 +105,26 @@ def connect_to_sql_server(host, user, password, port, database_name, query):
         if 'connection' in locals() and connection is not None:
             connection.close()
 
-def inject_ga():
-    GA_ID = "google_analytics"
-
-    # Note: Por favor, substitua o ID de "G-XXXXXXXXXX" pelo ID do seu aplicativo da web
-    # no Google Analytics. Você encontrará isso em sua conta do Google Analytics.
-
-    GA_JS = """
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
-    <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-20S9K4G7X6');
-    </script>
-    """
-
-    # Insira o script na tag head do modelo estático dentro do seu ambiente virtual
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    logging.info(f'editando {index_path}')
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID):  # se não encontrar a tag
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  # recupere a partir do backup
-        else:
-            shutil.copy(index_path, bck_index)  # mantenha um backup
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_JS)
-        index_path.write_text(new_html)
 
 def main():
     st.set_page_config(
         page_title="Tableau Poor",
-        layout="wide"
-    )
+        layout="wide")
 
     st.title("Tableau Poor - o seu software de Dataviz gratuito")
     
-    inject_ga()  # Injete o código do Google Analytics
+    takip="""
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-20S9K4G7X6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-20S9K4G7X6');
+</script>
+"""
+    components.html(takip, width=1, height=1)
 
     database = st.sidebar.selectbox("Selecione o banco de dados:", ["CSV/XLSX", "Postgres", "MySQL", "SQL Server"])
 
